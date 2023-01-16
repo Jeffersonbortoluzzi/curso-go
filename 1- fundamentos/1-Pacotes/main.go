3bb9b473717e9201be382a9aabe99701bebdcec4
Jeffersonbortoluzzi <jefferson.bortoluzzi@gmail.com>
2023-01-16 13:45:55 -0300

    pacotes externos

--- 1- fundamentos/1-Pacotes/main.go
+++ 1- fundamentos/1-Pacotes/main.go
@@ -2,10 +2,15 @@ package main
 
 import (
 	"fmt"
+	"github.com/badoux/checkmail"
 	"modulo/auxiliar"
 )
 
 func main() {
 	fmt.Println("Escrevendo do arquivo main")
 	auxiliar.Escrever()
+
+	erro := checkmail.ValidateFormat("jefferson.bortoluzzi@gmail.com")
+	fmt.Println(erro)
+	
 }
