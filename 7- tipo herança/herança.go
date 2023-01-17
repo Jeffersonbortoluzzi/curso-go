23fae821be3d95a8fabc4219c02553ec53b61c79
Jeffersonbortoluzzi <jefferson.bortoluzzi@gmail.com>
2023-01-17 16:04:44 -0300

    "herança"

---
+++ 7- tipo herança/herança.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+type pessoa struct {
+	nome      string
+	sobrenome string
+	idade     uint
+	altura    uint
+}
+
+type estudante struct {
+	pessoa
+	curso     string
+	faculdade string
+}
+
+func main() {
+
+	fmt.Println("Herança")
+
+	p1 := pessoa{"Jeff", "Bortoluzzi", 23, 196}
+	fmt.Println(p1)
+	
+	estudante1 := estudante{p1, "Engenharia Elétrica", "Anhanguera"}
+	fmt.Println(estudante1)
+	fmt.Println(estudante1.nome)
+	fmt.Println(estudante1.altura)
+
+}
