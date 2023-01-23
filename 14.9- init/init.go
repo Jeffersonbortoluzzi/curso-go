16e88ea4215968a19fb2ae48caadeaa24b0e7d33
Jeffersonbortoluzzi <jefferson.bortoluzzi@gmail.com>
2023-01-23 18:46:06 -0300

    init

--- 14.9- init/init.go
+++ 14.9- init/init.go
@@ -2,11 +2,14 @@ package main
 
 import "fmt"
 
+var n int
+
 func main() {
 	fmt.Println("Executando a função main")
-
+	fmt.Println(n)
 }
 
 func init() { // executa primeiro que a função main
 	fmt.Println("Executando a função init")
+	n = 10
 }
